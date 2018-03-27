0563e10b7e093d38b122abedaa9de4523ce56b38
Krista Capps <Krista@main-10-230-128-121.tamulink.tamu.edu>
2018-03-27 14:53:58 -0500

    added view tests and working on controller test coverage

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,1 +1,18 @@
 # spec/pages_controller_spec.rb
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :controller do
+    before do
+        @user = User.new()
+        @user.update({'uid' => '123456789'})
+        @pgCtrl = PagesController.new()
+        current_user = @user
+    end
+    describe "index" do
+        it "checks that default go to grader dashboard" do
+            current_user = @user
+            @pgCtrl.index
+            expect(request.fullpath).to eq(grader_dashboard_path)
+        end
+    end
+end
