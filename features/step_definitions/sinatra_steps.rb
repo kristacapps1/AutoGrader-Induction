5d5ad14d5b999a8cc9beec67fc92356d8e1546aa
DHLaurel <36649668+DHLaurel@users.noreply.github.com>
2018-04-12 17:16:16 -0500

    added google auth, not finished

--- features/step_definitions/sinatra_steps.rb
+++ features/step_definitions/sinatra_steps.rb
@@ -1,7 +1,11 @@
 Given (/^I am on the home page$/) do
   visit "/"
 end
- 
+
+Given (/^I am signed in with provider "(.*)"$/) do |provider|
+	vist "/auth/#{provider.downcase}"
+end
+
 Then (/^I should see "([^"]*)"$/) do |text|
   assert page.has_content?(text)
 end
@@ -9,7 +13,7 @@ end
 Then (/^I should see "([^"]*)" in the selector "([^"]*)"$/) do |text, selector|
   assert page.has_selector?(selector, content: text)
 end
- 
+
 Then (/^I should see "([^"]*)" in a link$/) do |text|
   assert page.has_link?(text)
 end
@@ -26,6 +30,6 @@ When (/^I click "([^"]*)"$/) do |element|
   click_on element
 end
 
-When (/^I select "([^"]*)" from "(^"]*)"$/) do |list, selection| 
+When (/^I select "([^"]*)" from "([^"]*)"$/) do |list, selection| 
   select(selection, :from => list)
 end
