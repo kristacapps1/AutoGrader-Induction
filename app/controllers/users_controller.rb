e1ed5862de236e17d2dda452f5ac2d6d54a5a775
EC2 Default User <ec2-user@ip-172-31-22-134.us-east-2.compute.internal>
2018-05-01 23:33:41 +0000

    encrypted uin fix/maybe update navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,57 +12,7 @@ class UsersController < ApplicationController
     def update_user
         @user = User.find(params[:id])
         
-        sum_grades = 0
-       d = 0;
-       if @user.a1grade.to_i >= 0
-           sum_grades = sum_grades + @user.a1grade.to_i
-           d = d + 1
-       end
-       if @user.a2grade.to_i >= 0
-           sum_grades = sum_grades + @user.a2grade.to_i
-           d = d + 1
-       end
-       if @user.a3grade.to_i >= 0
-           sum_grades = sum_grades + @user.a3grade.to_i
-           d= d + 1
-       end
-       if @user.a4grade.to_i >= 0
-           sum_grades = sum_grades + @user.a4grade.to_i
-           d = d + 1
-       end
-       if @user.a5grade.to_i >= 0
-           sum_grades = sum_grades + @user.a5grade.to_i
-           d = d + 1
-       end
-       if d > 0
-       @user.grade = sum_grades / d
-       end
        
-       
-       @user.save
-       
-       
-    a = 0
-    b = 0
-    c = 0
-    d = 0
-    f = 0
-    User.all.each do |x| 
-        if x.grade.to_i == -1
-        
-        elsif x.grade.to_i >= 90
-            a = a + 1
-        elsif x.grade.to_i >= 80 && x.grade.to_i < 90
-            b = b + 1
-        elsif x.grade.to_i >= 70 && x.grade.to_i < 80
-            c = c + 1
-        elsif x.grade.to_i >= 60 && x.grade.to_i < 70
-            d = d + 1
-        else
-            f = f + 1
-        end 
-    end
-    $grades = [a,b,c,d,f]
         
         
         if @user.update(user_params)
@@ -97,7 +47,7 @@ class UsersController < ApplicationController
     
     def user_params
         # NOTE: Using `strong_parameters` gem
-        params.require(:user).permit(:encrypted_tamu_uin, :class_section, :assignments, :tgrade, :premission, :tbasis, :tinduction, :tproof, :grade)
+        params.require(:user).permit(:encrypted_tamu_uin, :tamu_uin, :class_section, :assignments, :tgrade, :premission, :tbasis, :tinduction, :tproof, :grade)
         
     end
     
